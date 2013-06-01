b6334d6ecb25b3d8f8e56cd99d0eb6be75901e13
Drew Roos <droos@dimagi.com>
2013-06-01 10:05:34 -0400

    coordinate transformations and stuff

--- web/static/script/test.js
+++ web/static/script/test.js
@@ -3,7 +3,11 @@ var SCALE_OFFSET = [0, 0.5, 1, 1.5, 2, 3, 3.5, 4, 4.5, 5, 5.5, 6];
 var SCALE_LEN = Math.ceil(SCALE_OFFSET[SCALE_OFFSET.length - 1] + .5);
 var KEY0 = 21; // A
 var FREQ0 = 440. / Math.pow(2., 4.); // 4 octaves below A440
-var WIDTH_W = 30;
+var KEY_ASPECT = 6.;
+var BEAT_SZ = 6.;
+
+var NUM_WHITE_KEYS = 52;
+var TOTAL_KEYS = 88;
 
 function clock() {
     return new Date().getTime() / 1000.;
@@ -34,8 +38,8 @@ function PianoKey(i) {
 
     this.dim = function() {
         return {
-            width: this.white() ? WIDTH_W : .5 * WIDTH_W,
-            height: this.white() ? 150 : 90,
+            width: (this.white() ? 1. : .5),
+            height: KEY_ASPECT * (this.white() ? 1. : .6),
         };
     }
 
@@ -52,15 +56,16 @@ function PianoKey(i) {
         if (active) {
             this.last_on = clock();
         }
-        this.svg.attr({fill: active ? 'orange' : this.default_color()});
+        this.$svg.attr({fill: active ? 'orange' : this.default_color()});
     }
 
-    this.init_svg = function(ctx) {
+    this.init_svg = function(ctx, frame) {
         var dim = this.dim();
-        this.svg = ctx.rect(100 + this.center() * WIDTH_W - .5 * dim.width, 100, dim.width, dim.height)
-        this.svg.attr({fill: this.default_color()});
+        this.svg = ctx.rect(frame, this.center() + .5 - .5 * dim.width, KEY_ASPECT - dim.height, dim.width, dim.height);
+        this.$svg = $(this.svg);
+        this.$svg.attr({fill: this.default_color(), stroke: 'black', 'stroke-width': .05});
         var key = this;
-        this.svg.hover(function() {
+        this.$svg.hover(function() {
                 key.set_status(true);
                 key.cb(true);
             },
@@ -74,74 +79,31 @@ function PianoKey(i) {
 function init_keyboard() {
     $('#canvas').svg({onLoad:
             function(ctx) {
-                //var trainer = new TrainingSession(ctx);
-                //trainer.start();
-                init_pianoroll(ctx);
+                $.get('/pianoroll', function(data) {
+                        var trainer = new TrainingSession(ctx, data);
+                        trainer.start();
+                    });
             }
         });
 }
 
-function init_pianoroll(ctx) {
-    //var MODE = 'horiz';
-    var MODE = 'vert';
-
-    $.get('/pianoroll', function(data) {
-            var g = ctx.group({fill: '#ccf', stroke: 'black'});
-
-            $.each(data, function(k, v) {
-                    $.each(v, function(i, e) {
-                            var r;
-                            if (MODE == 'horiz') {
-                                var NOTE_SZ = 5;
-                                var BEAT_SZ = 25;
-                                r = ctx.rect(g, e.beat * BEAT_SZ, (127 - e.note) * NOTE_SZ, e.duration * BEAT_SZ, NOTE_SZ);
-                            } else {
-                                var BEAT_SZ = 100; //200;
-                                var y = function(b) {
-                                    return 950 - BEAT_SZ * b; // * (Math.log(b + 1) - Math.log(1.));
-                                }
-
-                                var NOTE_SZ = 1500 / 88.;
-
-                                /*
-                                if (e.beat > 50) {
-                                    return;
-                                }
-                                */
-
-                                r = ctx.rect(g, (e.note - 9) * NOTE_SZ, y(e.beat + e.duration), NOTE_SZ, y(e.beat) - y(e.beat + e.duration));
-                            }
-                        });
-                });
-
-            var blah = function(clock) {
-                $(g).attr('transform', 'translate(0, ' + (60. * clock * .001) + ')');
-                requestAnimationFrame(blah);
-            };
-            requestAnimationFrame(blah);
-        });
-
-}
-
-function init_trainer() {
-
-}
-
-function Keyboard(canvas) {
+function Keyboard(ctx, frame) {
     var kbd = this;
 
-    this.ctx = canvas;
+    this.ctx = ctx;
 
     this.init = function() {
+        this.frame = this.ctx.group(frame, {transform: 'scale(' + 1/NUM_WHITE_KEYS + ')'});
+    
         this.keys = [];
-        for (var i = 0; i < 88; i++) {
+        for (var i = 0; i < TOTAL_KEYS; i++) {
             this.keys.push(new PianoKey(i));
         }
         $.each([true, false], function(i, mode) {
                 // draw white keys first, then black
                 $.each(kbd.keys, function(i, e) {
                         if (e.white() == mode) {
-                            e.init_svg(kbd.ctx);
+                            e.init_svg(kbd.ctx, kbd.frame);
                         }
                     });
             });
@@ -179,26 +141,57 @@ function Keyboard(canvas) {
     this.init();
 }
 
-function PianoRoll() {
+function PianoRoll(ctx, frame, data) {
+    var pr = this;
 
+    this.init = function() {
+        var g = ctx.group(frame, {
+                transform: 'translate(0,' + (KEY_ASPECT/NUM_WHITE_KEYS) + ') scale(' + 1/TOTAL_KEYS + ')',
+                fill: '#ccf',
+                stroke: 'black',
+                'stroke-width': .05
+            });
+        this.pane = ctx.group(g);
+
+        $.each(data, function(k, v) {
+                $.each(v, function(i, e) {
+                        var r = ctx.rect(pr.pane, (e.note - 21), BEAT_SZ * e.beat, 1., BEAT_SZ * e.duration);
+                    });
+            });               
+    }
+
+    this.set_time = function(t) {
+        $(this.pane).attr('transform', 'translate(0, ' + -BEAT_SZ*t + ')');
+    }
+
+    this.init();
 }
 
-function TrainingSession(canvas) {
+function TrainingSession(ctx, data) {
     var trainer = this;
 
-    this.tempo = 120; // quarter notes per minute
+    this.tempo = 40; // quarter notes per minute
     this.clock = 0;
     this.last_started = null;
 
-    this.keyboard = new Keyboard(canvas);
-    this.pianoroll = null;
+    this.init = function() {
+        var w = $('#canvas').width();
+        var h = $('#canvas').height();
+        var g = ctx.group({transform: 'scale(' + w + ',' + -w + ') translate(0,' + (-h/w) + ')'});
 
-    this.tick = function() {
-        console.log(this.keyboard.currently_pressed());
+        this.pianoroll = new PianoRoll(ctx, g, data);
+        this.keyboard = new Keyboard(ctx, g);
+    }
+
+    this.tick = function(t) {
+        this.pianoroll.set_time(t / 60. * this.tempo);
     }
 
     this.start = function() {
-        var TICK = 1.; //0.01;
-        //setInterval(function() { trainer.tick(); }, 1000. * TICK);
+        var TICK = 0.01;
+        var t0 = clock();
+        setInterval(function() { trainer.tick(clock() - t0); }, 1000. * TICK);
     }
+
+    this.init();
 }
