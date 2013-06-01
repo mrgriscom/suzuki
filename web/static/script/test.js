8398fa8a2c89a593c0137640f48028cfab78a04c
Drew Roos <droos@dimagi.com>
2013-06-01 14:06:13 -0400

    interactive!

--- web/static/script/test.js
+++ web/static/script/test.js
@@ -13,11 +13,12 @@ function clock() {
     return new Date().getTime() / 1000.;
 }
 
-function PianoKey(i) {
+function PianoKey(i, get_beat) {
     this.i = i;
-    this.status = false;
+    this.pressed = false;
+    this.expected = false;
     this.last_on = null;
-    this.cb = function(status) {};
+    //this.cb = function(status) {};
 
     this.white = function() {
         return (this.offset() % 1. == 0.);
@@ -51,12 +52,31 @@ function PianoKey(i) {
         return FREQ0 * Math.pow(2., this.i / SCALE_OFFSET.length)
     }
 
-    this.set_status = function(active) {
-        this.status = active;
-        if (active) {
-            this.last_on = clock();
+    this.set_pressed = function(pressed) {
+        this.pressed = pressed;
+        if (pressed) {
+            this.last_on = get_beat();
         }
-        this.$svg.attr({fill: active ? 'orange' : this.default_color()});
+        this.refresh();
+    }
+
+    this.set_expected = function(expected) {
+        this.expected = expected;
+        this.refresh();
+    }
+
+    this.refresh = function() {
+        var color;
+        if (this.pressed && this.expected) {
+            color = '#0f0';
+        } else if (this.pressed && !this.expected) {
+            color = '#f88';
+        } else if (this.expected && !this.pressed) {
+            color = 'orange';
+        } else {
+            color = this.default_color();
+        }
+        this.$svg.attr({fill: color});
     }
 
     this.init_svg = function(ctx, frame) {
@@ -65,6 +85,7 @@ function PianoKey(i) {
         this.$svg = $(this.svg);
         this.$svg.attr({fill: this.default_color(), stroke: 'black', 'stroke-width': .05});
         var key = this;
+        /*
         this.$svg.hover(function() {
                 key.set_status(true);
                 key.cb(true);
@@ -73,6 +94,7 @@ function PianoKey(i) {
                 key.set_status(false);
                 key.cb(false);
             });
+        */
     }
 }
 
@@ -87,7 +109,7 @@ function init_keyboard() {
         });
 }
 
-function Keyboard(ctx, frame) {
+function Keyboard(ctx, frame, get_beat) {
     var kbd = this;
 
     this.ctx = ctx;
@@ -97,7 +119,7 @@ function Keyboard(ctx, frame) {
     
         this.keys = [];
         for (var i = 0; i < TOTAL_KEYS; i++) {
-            this.keys.push(new PianoKey(i));
+            this.keys.push(new PianoKey(i, get_beat));
         }
         $.each([true, false], function(i, mode) {
                 // draw white keys first, then black
@@ -110,11 +132,13 @@ function Keyboard(ctx, frame) {
 
         this.conn = new WebSocket('ws://' + window.location.host + '/socket');
         this.conn.onopen = function () {
+            /*
             $.each(kbd.keys, function(i, e) {
                     e.cb = function(status) {
                         kbd.conn.send(JSON.stringify({status: status ? 'on' : 'off', note: this.key()}));
                     };
                 });
+            */
         };
         this.conn.onerror = function (error) {
             console.log('websocket error ' + error);
@@ -123,7 +147,7 @@ function Keyboard(ctx, frame) {
             var data = JSON.parse(e.data);
             $.each(data, function(i, evt) {
                     var key = kbd.keys[evt.note - KEY0];
-                    key.set_status(evt.state == 'on');
+                    key.set_pressed(evt.state == 'on');
                 });
         };
     }
@@ -131,11 +155,20 @@ function Keyboard(ctx, frame) {
     this.currently_pressed = function() {
         var pressed = {};
         $.each(this.keys, function(i, e) {
-                if (e.status) {
-                    pressed[e.i] = e.last_on;
+                if (e.pressed) {
+                    pressed[e.i + KEY0] = e.last_on;
                 }
             });
         return pressed;
+    }
+
+    this.set_expected = function(exp) {
+        $.each(this.keys, function(i, e) {
+                e.set_expected(false);
+            });
+        $.each(exp, function(k, v) {
+                kbd.keys[+k - KEY0].set_expected(true);
+            });
     }
 
     this.init();
@@ -147,7 +180,6 @@ function PianoRoll(ctx, frame, data) {
     this.init = function() {
         var g = ctx.group(frame, {
                 transform: 'translate(0,' + (KEY_ASPECT/NUM_WHITE_KEYS) + ') scale(' + 1/TOTAL_KEYS + ')',
-                fill: '#ccf',
                 stroke: 'black',
                 'stroke-width': .05
             });
@@ -155,13 +187,30 @@ function PianoRoll(ctx, frame, data) {
 
         $.each(data, function(k, v) {
                 $.each(v, function(i, e) {
-                        var r = ctx.rect(pr.pane, (e.note - 21), BEAT_SZ * e.beat, 1., BEAT_SZ * e.duration);
+                        var white = new PianoKey(e.note - KEY0).white();
+                        var BLACK_MARGIN = 0.2;
+                        var r = ctx.rect(pr.pane, e.note - KEY0 + (white ? 0. : BLACK_MARGIN), BEAT_SZ * e.beat, 1. - (white ? 0. : 2*BLACK_MARGIN), BEAT_SZ * e.duration, {
+                                fill: (white ? '#ccf' : '#88b'),
+                                //fill: (white ? '#fcf' : '#b8b'),
+                            });
                     });
-            });               
+            });
     }
 
     this.set_time = function(t) {
         $(this.pane).attr('transform', 'translate(0, ' + -BEAT_SZ*t + ')');
+    }
+
+    this.expected = function(t) {
+        var exp = {};
+        $.each(data, function(k, v) {
+                $.each(v, function(i, e) {
+                        if (e.beat <= t && t < e.beat + e.duration) {
+                            exp[e.note] = e.beat;
+                        }
+                    });
+            });
+        return exp;
     }
 
     this.init();
@@ -171,8 +220,8 @@ function TrainingSession(ctx, data) {
     var trainer = this;
 
     this.tempo = 40; // quarter notes per minute
-    this.clock = 0;
-    this.last_started = null;
+    this.cur_beat = 0.;
+    this.cur_clock = 0.;
 
     this.init = function() {
         var w = $('#canvas').width();
@@ -180,17 +229,57 @@ function TrainingSession(ctx, data) {
         var g = ctx.group({transform: 'scale(' + w + ',' + -w + ') translate(0,' + (-h/w) + ')'});
 
         this.pianoroll = new PianoRoll(ctx, g, data);
-        this.keyboard = new Keyboard(ctx, g);
+        this.keyboard = new Keyboard(ctx, g, function() { return trainer.cur_beat; });
     }
 
     this.tick = function(t) {
-        this.pianoroll.set_time(t / 60. * this.tempo);
+        var pressed = this.keyboard.currently_pressed();
+        var expected = this.pianoroll.expected(this.cur_beat);
+
+        this.keyboard.set_expected(expected);
+        this.pianoroll.set_time(this.cur_beat);
+
+        var correct = this.correct_keys(pressed, expected);
+        if (correct) {
+            this.cur_beat += (t - this.cur_clock) / 60. * this.tempo;
+        }
+        this.cur_clock = t;
     }
+
+    this.correct_keys = function(pressed, expected) {
+        var _pressed = [];
+        var _expected = [];
+        $.each(pressed, function(k, v) {
+                _pressed.push(+k);
+            });
+        $.each(expected, function(k, v) {
+                _expected.push(+k);
+            });
+        _pressed = _.sortBy(_pressed, function(i) { return i; });
+        _expected = _.sortBy(_expected, function(i) { return i; });
+        if (_pressed.length == _expected.length) {
+            for (var i = 0; i < _pressed.length; i++) {
+                if (_pressed[i] != _expected[i]) {
+                    return false;
+                }
+                var last_pressed = pressed[_pressed[i]];
+                var last_expected = expected[_expected[i]];
+                if (last_pressed < last_expected) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    };
 
     this.start = function() {
         var TICK = 0.01;
         var t0 = clock();
-        setInterval(function() { trainer.tick(clock() - t0); }, 1000. * TICK);
+        setInterval(function() {
+                trainer.tick(clock() - t0);
+            }, 1000. * TICK);
     }
 
     this.init();
